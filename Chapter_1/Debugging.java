4ead3f1d331e9d523f49e5a08a0cf6c9541838ae
NinoJaphethSantizo <saimongheart1@gmail.com>
2025-09-01 14:27:50 +0800

    Add files via upload

---
+++ Chapter_1/Debugging.java
@@ -0,0 +1,19 @@
+
+public class Debugging {
+    // This program displays a greeting
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } 
+
+}
+ /* 
+ List down the spotted bugs below:
+e.g
+
+1. Class name has a space →
+2. Method name should be lowercase → Main → should be main
+3. Incorrect parameter → String args → should be String[] args
+4. Typo → Systm → should be System
+5. Missing semicolon → after System.out.println("Hello World!")
+6.Missing closing brace → for the class
+ */
