ea63c47d538b9e4e682dc010ff705fa92b5421e2
NiñoJaphethSantizo-UCM <saimongheart1@gmail.com>
2025-10-27 16:43:58 +0800

    Update CodingBankBalanceByRateAndYear.java

--- Chapter_6/CodingBankBalanceByRateAndYear.java
+++ Chapter_6/CodingBankBalanceByRateAndYear.java
@@ -5,18 +5,18 @@ public class CodingBankBalanceByRateAndYear {
         Scanner input = new Scanner(System.in);
         double balance;
 
-        // Ask user for initial bank balance
+       
         System.out.print("Enter initial bank balance > ");
         balance = input.nextDouble();
         System.out.println();
 
-        // Outer loop for interest rates (from 0.02 to 0.05)
+       
         for (double rate = 0.02; rate <= 0.05; rate += 0.01) {
             double currentBalance = balance;
 
             System.out.printf("With an initial balance of $%.1f at an interest rate of %.2f%n", balance, rate);
 
-            // Inner loop for years (1 to 4)
+            
             for (int year = 1; year <= 4; year++) {
                 currentBalance += currentBalance * rate;
                 System.out.printf("After year %d balance is $%.4f%n", year, currentBalance);
@@ -28,3 +28,4 @@ public class CodingBankBalanceByRateAndYear {
         input.close();
     }
 }
+
